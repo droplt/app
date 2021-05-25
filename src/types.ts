4065f7cb5eb0cf9f74fae676c8b9132d94fd13eb
François Lefebvre <francois.lfe@gmail.com>
2021-05-25 13:50:47 +0200

    chore(codegen): setup

---
+++ src/types.ts
@@ -0,0 +1,43 @@
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> &
+  { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> &
+  { [SubKey in K]: Maybe<T[SubKey]> };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  /** The javascript `Date` as string. Type represents date and time as the ISO Date string. */
+  DateTime: any;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  user: UserModel;
+  users: Array<UserModel>;
+};
+
+export type QueryUserArgs = {
+  uid?: Maybe<Scalars['String']>;
+};
+
+export type UserModel = {
+  __typename?: 'UserModel';
+  activeAt?: Maybe<Scalars['DateTime']>;
+  avatarUrl?: Maybe<Scalars['String']>;
+  connectedAt?: Maybe<Scalars['DateTime']>;
+  createdAt?: Maybe<Scalars['DateTime']>;
+  email?: Maybe<Scalars['String']>;
+  isAdmin?: Maybe<Scalars['Boolean']>;
+  isDisabled?: Maybe<Scalars['Boolean']>;
+  isVerified?: Maybe<Scalars['Boolean']>;
+  phone?: Maybe<Scalars['String']>;
+  uid: Scalars['ID'];
+  username?: Maybe<Scalars['String']>;
+};
