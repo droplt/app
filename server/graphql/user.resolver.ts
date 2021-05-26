8721bd6312768ab7cd68ad536d45f0fdce381e11
François Lefebvre <francois@shine.fr>
2021-05-26 14:52:27 +0200

    chore(project): setup

--- server/graphql/user.resolver.ts
+++ server/graphql/user.resolver.ts
@@ -1,9 +1,10 @@
-import { Arg, Query, Resolver } from 'type-graphql';
+import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 
 import { UserModel } from '../entities';
-import { toUserModel } from '../helpers/user';
+import { toUpdateRequest, toUserModel } from '../helpers/user';
 import admin from '../services/firebase';
 import { AuthUser, AuthUserType } from './decorators';
+import { UpdateUserInput } from './inputs';
 
 @Resolver(() => UserModel)
 export class UserResolver {
@@ -20,8 +21,17 @@ export class UserResolver {
   }
 
   @Query(() => [UserModel])
-  async users() {
+  async users(): Promise<UserModel[]> {
     const list = await admin.auth().listUsers(1000);
-    return list.users.map(toUserModel);
+    return list.users.map((user) => toUserModel(user));
+  }
+
+  @Mutation(() => UserModel)
+  async updateUser(
+    @Arg('uid') uid: string,
+    @Arg('data') updates: UpdateUserInput
+  ): Promise<UserModel> {
+    const user = await admin.auth().updateUser(uid, toUpdateRequest(updates));
+    return toUserModel(user);
   }
 }
--- server/helpers/user/index.ts
+++ server/helpers/user/index.ts
@@ -1,2 +1,3 @@
 export { default as getUserRoles } from './getUserRoles';
+export { default as toUpdateRequest } from './toUpdateRequest';
 export { default as toUserModel } from './toUserModel';
