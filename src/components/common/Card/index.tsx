48e7fc4f70ec41a5dc6af62ff73f5f8e57cf32da
François Lefebvre <francois.lfe@gmail.com>
2021-05-27 00:02:48 +0200

    chore(project): setup

--- src/components/common/Card/index.tsx
+++ src/components/common/Card/index.tsx
@@ -1,6 +1,13 @@
 import classNames from 'classnames';
 
-const cardClass = classNames('border', 'border-gray-300', 'rounded-2xl', 'p-4');
+const cardClass = classNames(
+  'border',
+  'shadow',
+  'bg-white',
+  'border-gray-300',
+  'rounded-2xl',
+  'p-4'
+);
 
 const Card: React.FC = ({ children }) => {
   return <div className={cardClass}>{children}</div>;
