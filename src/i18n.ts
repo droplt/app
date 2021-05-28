1715bc248852664b8f82c05dddd0e200f0852bc8
François Lefebvre <francois.lfe@gmail.com>
2021-05-28 22:44:12 +0200

    chore(project): setup

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,9 +2,6 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
 const resources = {
   en: {
     translation: {
@@ -23,18 +20,13 @@ const resources = {
 };
 
 i18n
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
-  // passes i18n down to react-i18next
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     resources,
-    lng: 'fr', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-
+    detection: {
+      order: ['navigator'],
+    },
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
